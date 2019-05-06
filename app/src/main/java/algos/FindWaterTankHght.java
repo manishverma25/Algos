0a2d6fe12f73b949f1c6bf8052872eb382ba499f
manishverma <manishverma25@gmail.com>
2019-05-06 15:45:22 +0530

    conatiner water tank max height prblm using dp

---
+++ app/src/main/java/algos/FindWaterTankHght.java
@@ -0,0 +1,39 @@
+package algos;
+
+
+class FindWaterTankHght{
+
+    public static int maxArea(int A[], int len)
+    {
+        int l = 0;
+        int r = len -1;
+        int area = 0;
+
+        while (l < r)
+        {
+            // Calculating the max area
+            area = Math.max(area,
+                    Math.min(A[l], A[r]) * (r - l));
+
+            if (A[l] < A[r])
+                l += 1;
+
+            else
+                r -= 1;
+        }
+        return area;
+    }
+
+    public static void main(String[] args)
+    {
+        int a[] = {10,5,9,2};
+        int b[] = {3, 1, 2, 4, 5};
+
+
+        int len1 = 4;
+        System.out.print( maxArea(a, len1)+"\n" );
+
+        int len2 = 5;
+//        System.out.print( maxArea(b, len2) );
+    }
+}

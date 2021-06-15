791cc5f390445aff0b91278a17482bd5b66fc965
manishverma <manishverma25@gmail.com>
2021-06-15 18:43:26 +0530

    ch2 remove duplicate nodes

---
+++ app/src/main/java/algos/cracking/coding/book/Ch2/Q2_02_Return_Kth_To_Last/Tester.java
@@ -0,0 +1,12 @@
+package algos.cracking.coding.book.Ch2.Q2_02_Return_Kth_To_Last;
+
+import algos.cracking.coding.book.CtCILibrary.LinkedListNode;
+
+public class Tester {
+
+	
+	public static void main(String[] args) {
+
+	}
+
+}

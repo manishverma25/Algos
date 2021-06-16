76a1ebdcb12d9d81c31137a4071ac853074a8c93
manishverma <manishverma25@gmail.com>
2021-06-16 15:31:22 +0530

    ch2 remove duplicate nodes other new  apprach

--- app/src/main/java/algos/cracking/coding/book/Ch2/Q2_01_Remove_Dups/QuestionB.java
+++ app/src/main/java/algos/cracking/coding/book/Ch2/Q2_01_Remove_Dups/QuestionB.java
@@ -19,16 +19,41 @@ public class QuestionB {
 	}	
 	
 	public static void main(String[] args) {	
-		LinkedListNode first = new LinkedListNode(0, null, null); //AssortedMethods.randomLinkedList(1000, 0, 2);
+		LinkedListNode first = new LinkedListNode(1, null, null); //AssortedMethods.randomLinkedList(1000, 0, 2);
 		LinkedListNode head = first;
 		LinkedListNode second = first;
+		int data []= new int [] {1,2,1,1,2,5,1,7};
 		for (int i = 1; i < 8; i++) {
-			second = new LinkedListNode(i % 2, null, null);
+//			second = new LinkedListNode(i % 2, null, null);
+			second = new LinkedListNode(data[i], null, null);
 			first.setNext(second);
 			second.setPrevious(first);
 			first = second;
 		}
 		System.out.println(head.printForward());
-		deleteDups(head);
+//		deleteDups(head);
+
+		deleteDupsNew(head);
+		System.out.println(head.printForward());
+	}
+
+	public static void deleteDupsNew(LinkedListNode head) {
+		LinkedListNode current = head;
+		while (current != null) {
+			/* Remove all future nodes that have the same value */
+			LinkedListNode runner = current.next;
+			while (runner != null) {
+				if (runner.data == current.data) {
+					LinkedListNode temp= runner.next;
+					runner.prev.next = temp;
+					if(temp != null){
+						temp.prev = runner.prev;
+					}
+
+				}
+				runner = runner.next;
+			}
+			current = current.next;
+		}
 	}
 }

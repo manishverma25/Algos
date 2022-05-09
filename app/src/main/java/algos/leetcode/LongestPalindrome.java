db41aeeb233d0c002300c2b41773001efcb6c39a
Manish.Verma1 <manishverma25@gmail.com>
2022-05-09 17:40:16 +0530

    longest  substring palidrom DP

--- app/src/main/java/algos/leetcode/LongestPalindrome.java
+++ app/src/main/java/algos/leetcode/LongestPalindrome.java
@@ -5,12 +5,62 @@ public class LongestPalindrome {
 
     public static void main(String [] s){
 
-        String data = "aba";
+        String data = "abcbaqp";
 
-       String r=  longestPalindrome(data);
-       System.out.print(" longestPalindrome "+r);
+//       String r=  longestPalindrome(data);
+       String r=  longestPalidromeStringDP(data);
+       System.out.print(">>>>ln longestPalindrome "+r);
 
     }
+
+    public static String longestPalidromeStringDP(String s){
+        int [][] dpLPS = new int [s.length()][s.length()];
+        int solutionRow  = 0;
+        int solutionCol  = 0;
+        String longestPalidromeStr = "";
+
+       int i =0;int j = 0;
+        for(int c = 0; c < dpLPS.length  ;i++,j++, c++){ // a
+            dpLPS[i][j] = 1;
+        } //
+        for(int c = 1; c < dpLPS.length  ; c++){ // a
+            int row =0;
+            for(int col = c; col < dpLPS.length  ; col++,row++){ // a
+                System.out.println(" row : "+row+ " (s.charAt(row) : "+ (s.charAt(row))+    " col : "+col +"  (s.charAt(col))  : "+ (s.charAt(col)));
+               char left =  s.charAt(row);
+                char rght =  s.charAt(col);
+                int subRow  = row +1;
+                int subcol = col -1;
+                System.out.println(" left : "+left+ " rght: "+ rght);
+                if(left == rght ){ // 1
+                    if( (subRow <= subcol )  && dpLPS[row+1][col-1] > 0){
+                        dpLPS[row][col]   =  dpLPS[row+1][col-1] +2;
+                        System.out.println(" inside   dpLPS[row+1][col-1]  : "+dpLPS[row+1][col-1]  +" row "+row+ "col :"+col + "  longestPalidromeStr :"+longestPalidromeStr);
+                        if( col - row +1 > longestPalidromeStr.length()){
+                            longestPalidromeStr = s.substring(row,col+1);
+                            solutionRow = row;
+                            solutionCol = col;
+                            System.out.println(" updating longestPalidromeStr  : "+longestPalidromeStr  +" row "+row+ "col :"+col);
+                        }
+                    }
+                }// 1
+            }
+        } //a
+        System.out.println(" final  longestPalidromeStr  : "+longestPalidromeStr  +" solutionRow "+solutionRow+ " solutionCol  :"+solutionCol);
+        return longestPalidromeStr;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
     public static String longestPalindrome(String s) {
         if (s == null || s.length() < 1) return "";
@@ -35,6 +85,9 @@ public class LongestPalindrome {
         }
         return R - L - 1;
     }
+
+
+
 
 
 
